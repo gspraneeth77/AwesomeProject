737a575ca9fd771d7fa658ff784b7d5483ab612f
vn53d79 <sai.praneeth.gorintala@walmart.com>
2022-03-15 00:46:09 -0400

    Package list added in application

--- android/sdk/src/main/java/com/walmart/sdk/MainApplication.java
+++ android/sdk/src/main/java/com/walmart/sdk/MainApplication.java
@@ -2,7 +2,7 @@ package com.walmart.sdk;
 
 import android.app.Application;
 import android.content.Context;
-
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -24,8 +24,11 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          return Collections.singletonList(new MyReactPackage());
+           packages.add(new MyReactPackage());
+          return packages;
         }
 
         @Override
